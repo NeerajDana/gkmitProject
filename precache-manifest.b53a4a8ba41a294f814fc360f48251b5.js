56963ea475f3fceac669f2930451c90aec2cae41
Neeraj Dana <neerajdana9@gmail.com>
2020-04-02 18:46:01 +0530

    gh-pages

---
+++ precache-manifest.b53a4a8ba41a294f814fc360f48251b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8cc3fda8e1cae551fcbdf81986a392a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afb8d89079c5e3eb6d7f",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "b4ad4e84d73c27ff567f",
+    "url": "/static/css/main.4e6d22b8.chunk.css"
+  },
+  {
+    "revision": "afb8d89079c5e3eb6d7f",
+    "url": "/static/js/2.be93f5fe.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/static/js/2.be93f5fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ad4e84d73c27ff567f",
+    "url": "/static/js/main.80c1a347.chunk.js"
+  },
+  {
+    "revision": "9ba6d7daa45235e69c1c",
+    "url": "/static/js/runtime-main.75b92534.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
